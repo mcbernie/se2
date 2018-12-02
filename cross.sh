3c4c55362b730d32b2f1f93ffc67bb19003a29b0
Nicolas Brüggemann <nbrueggemann@itcoops.de>
2018-12-02 13:13:07 +0100

    rename cross script

--- cross.sh
+++ cross.sh
@@ -4,5 +4,5 @@ docker run \
     --volume ~/.cargo/registry:/home/cross/.cargo/registry \
     --volume ~/ownCloud/Projekte/StarEntertainer/rust/se_2/deb_cross:/home/cross/deb-deps \
     rust-pi-cross build \
-        --release --features=ffmpeg_rpi_zero_special
+        --features=ffmpeg_rpi_zero_special
     
